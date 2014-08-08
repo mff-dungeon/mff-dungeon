eb69b5c7929e672f13b4c512c91817420fd45c26
Salmelu <salmelu@gmail.com>
2014-08-08 18:11:23 +0200

    More cleaning of the DatabaseHandler + Relations, adding docs

--- src/Relation.hpp
+++ src/Relation.hpp
@@ -4,7 +4,7 @@
 #ifndef RELATION_HPP
 #define RELATION_HPP
 
-/*
+/**
  * Object representing a relation row in the DB.
  *
  * It is used mainly for easier and more comprehensible transfer of relations
@@ -23,9 +23,36 @@ namespace Dungeon {
 		objId pid, sid;
 		string pclass, sclass, relation;
 
+		/**
+		 * Constructs a new relation. May be supplied with empty string 
+		 * considering them as wildcards (*) for the database queries
+         * @param pid objectId of the parent object
+         * @param sid objectId of the son object
+         * @param pclass className of the parent object
+         * @param sclass className of the son object
+         * @param relation the identifier of the relation
+         */
 		Relation(objId pid, objId sid, string pclass,
 			string sclass, string relation);
-		string getWhere();
+		
+		/**
+		 * Forms a select query matching the member variables of the object
+         * @return a string containing a database query
+         */
+		string getSelectQuery();
+		
+		/**
+		 * Forms a delete query matching the member variables of the object
+         * @return a string containing a database query
+         */
+		string getDeleteQuery();
+	private:
+		/**
+		 * Private method for creating the common WHERE part of the query
+         * @param query stream to insert query into
+         * @return validity of the WHERE clause
+         */
+		bool addWhere(stringstream& query);
 	};
 }
 
